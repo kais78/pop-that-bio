2b384ed0a1a7e61abdf10a1f0d7988824c072231
kais <kais2020@yahoo.com>
2020-01-20 15:15:11 +0100

    adding footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,16 @@
   <div class="container-fluid pt-5">
       <div class="row d-flex align-items-center">
         <div class="col-md-12 col-lg-12">
+        
+      <div>
+        <i class="fab fa-facebook"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-youtube"></i>
+      </div>
+
           <p class="text-center text-white pt-5 mt-5">© 2020 MOVIES</p>
+         
+            
         </div>
       </div>
 
@@ -19,11 +28,16 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style scoped lang="css">
 .container-fluid{
   height: 200px;
   background: #2672ffb6;
   background: linear-gradient(to bottom, #24243e7a, #302b63, #15026bdc); 
 }
 
+.fab{
+  font-size: 24px;
+  color: #fff;
+  padding:1%;
+}
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>POP-THAT-BIO</h1>
   </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="css">
+.about{
+  -webkit-text-fill-color: aliceblue;
+  background: #2672ffb6;
+  background: linear-gradient(to top, #24243e7a, #302b63, #15026bdc); 
+}
+</style>
